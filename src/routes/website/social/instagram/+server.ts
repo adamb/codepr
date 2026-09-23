abd6bebfa45b41f8c45f6b723dca339eee8ff65e
Adam Beguelin <beguelin@gmail.com>
2026-09-23 19:58:49 -0400

    fix: point Instagram footer and redirect to code_puertorico

--- src/routes/website/social/instagram/+server.ts
+++ src/routes/website/social/instagram/+server.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
 
 export function GET() {
-	return redirect(302, 'https://instagram.com/codepr');
+	return redirect(302, 'https://instagram.com/code_puertorico');
 }
